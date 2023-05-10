03fbad1c840355d728b0eb90ffadb042a2ca940f
rodqui <110335551+rodqui@users.noreply.github.com>
2023-05-10 22:19:01 +0200

    update

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const port =  3000;
+
+const https = require("node:https"); //this to use APIS
+
+
+
+
+/********************/
+var dirname = __dirname;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+/**********************/
+
+
+app.get("/", (req, res)=>{
+
+    res.sendFile(dirname+"/index.html");
+    
+});
+
+
+app.post("/new", (req, res)=>{
+    
+    const cityName = req.body.cityName;
+    const apiKey = "YOUR API KEY";
+    const url = "https://api.openweathermap.org/data/2.5/weather?lang=en&q="+cityName+"&appid="+apiKey+"&units=metric";
+    //https.get function call back returns result of the petition
+    https.get(url, (response)=>{
+      
+
+        //response.on: this metod returns the contains of API format JSON
+        response.on("data",(d)=>{
+            //process.stdout.write(d);
+            const weatherData = JSON.parse(d); //transform from JSON FORMAT to Javascript Object
+            console.log("est: "+ d);
+
+           
+           
+            res.send(weatherData);
+        });
+
+
+    });
+
+});
+
+app.use(express.static(dirname + "/"));
+
+
+app.listen(port, ()=>{
+    console.log(`App listening on port ${port}`);
+});
---
+++ script.js
@@ -0,0 +1,66 @@
+$(function(){
+
+
+    $("#cardWeather").hide();
+
+
+    $("#cityForm").on("submit", function(e){
+        e.preventDefault();
+
+        var city = $("#cityName");
+        var data = {};
+        data.cityName = city.val();
+        
+        $("#windCard").html("<i class='fa-solid fa-wind'></i>");
+        $("#humidityCard").html("<i class='fa-solid fa-water'></i>");
+        $("#pressureCard").html("<i class='fa-solid fa-circle-exclamation'></i>");
+
+        
+
+        $.ajax({
+            url: "http://localhost:3000/new",
+            type: "post",
+            
+            data: JSON.stringify(data),
+            cache: false,
+            contentType: 'application/json',
+            success: function(data){
+             
+              
+                if(data.cod!==200){
+                    $("#errorCard").show().fadeIn();
+                    $("#card").hide();
+                    $("#cardWeather").hide().fadeIn();
+                   
+                }else{
+
+                    $("#cityNameCard").html(data.name);
+                    $("#degreesCard").html(data.main.temp+"ºC");
+
+                    var today = new Date();
+                    var now = today.toLocaleDateString("es-ES");
+                    
+                    $("#hourCard").html(now);
+
+
+
+                    var icon = data.weather[0].icon;
+                    $("#imgCard").attr("src","http://openweathermap.org/img/wn/"+icon+"@2x.png");
+
+                    $("#descriptionCard").html(data.weather[0].description);
+
+                    $("#windCard").append("&nbsp;&nbsp;&nbsp;"+data.wind.speed);
+                    $("#humidityCard").append("&nbsp;&nbsp;&nbsp;"+data.main.humidity);
+                    $("#pressureCard").append("&nbsp;&nbsp;&nbsp;"+data.main.pressure);
+                    $("#errorCard").hide();
+                    $("#cardWeather").hide().fadeIn();
+                    $("#card").show().fadeIn();
+                }
+            }
+        })
+
+        
+
+    });
+
+});
